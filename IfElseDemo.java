6e854552068db307ad2a91012809a2dac159591d
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-03 08:15:34 -0700

    Add files via upload

---
+++ IfElseDemo.java
@@ -0,0 +1,19 @@
+package com.topics.coreJava;
+
+// Demonstration about use of if / else statements
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		int age = 28;
+		if (age < 28) {
+			System.out.println("Age is less than 28");
+		}
+		else if (age > 28) {
+			System.out.println("Age is greater than 28");
+		}
+		else {
+			System.out.println("Age is equal to 28");
+		}
+	}
+
+}
