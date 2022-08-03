8d73455d2009f31bd1ef9a59b9bf25fdefe9b708
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-03 04:09:59 -0700

    Add files via upload

---
+++ SwitchCaseDemo.java
@@ -0,0 +1,51 @@
+package com.topics.coreJava;
+
+// Program to show the use of switch case to display the month based on the number assigned.
+public class SwitchCaseDemo {
+
+	public static void main(String[] args) {
+		int month = 13;
+		switch(month) {
+			case 1:
+				System.out.println("Current Month: January");
+			break;
+			case 2:
+				System.out.println("Current Month: February");
+			break;
+			case 3:
+				System.out.println("Current Month: March");
+			break;
+			case 4:
+				System.out.println("Current Month: April");
+			break;
+			case 5:
+				System.out.println("Current Month: May");
+			break;
+			case 6:
+				System.out.println("Current Month: June");
+			break;
+			case 7:
+				System.out.println("Current Month: July");
+			break;
+			case 8:
+				System.out.println("Current Month: August");
+			break;
+			case 9:
+				System.out.println("Current Month: September");
+			break;
+			case 10:
+				System.out.println("Current Month: October");
+			break;
+			case 11:
+				System.out.println("Current Month: November");
+			break;
+			case 12:
+				System.out.println("Current Month: December");
+			break;
+			default:
+				System.out.println("Invalid input - Wrong month number.");
+			break;
+		}
+	}
+
+}
