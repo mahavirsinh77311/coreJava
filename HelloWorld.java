91a719a6c30f6514e983c1d330d7f26341194460
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-02 23:29:31 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package com.topics.coreJava;
+
+//  class which demonstrate to print Hello World 
+public class HelloWorld 
+{
+
+	public static void main(String[] args)
+	{
+
+		System.out.println("Hello World");
+
+	}
+
+}
