ea2b69679c01c4de30f131242ea0cb614780f552
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-03 04:10:27 -0700

    Add files via upload

---
+++ WhileLoopDemo.java
@@ -0,0 +1,18 @@
+package com.topics.coreJava;
+
+// Showing the use of while loop  
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// Loop counter initialisation
+		int i = 1;
+		
+		// Loop to print 1 to 10.
+		while(i <= 10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
