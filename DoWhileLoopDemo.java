18bd0e26ca2ee6d7e5e1d3166e64884eaf1dba82
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-03 06:41:34 -0700

    Add files via upload

---
+++ DoWhileLoopDemo.java
@@ -0,0 +1,17 @@
+package com.topics.coreJava;
+
+// Showing the use of do while loop
+public class DoWhileLoopDemo {
+
+	public static void main(String[] args) {
+		// Initialization
+		int i = 1;
+		// Loop to print 1 to 10 number.
+		do
+		{
+			System.out.println(i);
+			i++;
+		} while (i <= 10);
+	}
+
+}
