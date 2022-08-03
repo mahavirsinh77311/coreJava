614adcda2ad3b417939f56a6a29718c4ce16a048
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-03 06:45:28 -0700

    Add files via upload

---
+++ ConstructorDemo.java
@@ -0,0 +1,26 @@
+package com.topics.coreJava;
+
+// Program to demonstrate default and parameterized constructor
+public class ConstructorDemo {
+
+	int i;
+	int j;
+	// Default constructor
+	ConstructorDemo() {
+		System.out.println("In default constructor");
+	}
+	// Parameterized constructor
+	ConstructorDemo(int x, int y) {
+		i = x;
+		j = y;
+		int z = i / j;
+		System.out.println(z);
+	}
+	public static void main(String[] args) {
+		// invoke default constructor
+		ConstructorDemo con = new ConstructorDemo();
+		// invoke Parameterized constructor
+		ConstructorDemo cd = new ConstructorDemo(10, 5);
+	}
+
+}
