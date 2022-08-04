7b5f28256415210842e3ead798b9ad3c6a533f0f
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-03 21:40:10 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package com.topics.coreJava;
+
+/**
+* <h1>Hello, World!</h1>
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+* @author  mahavirsinh parmar
+* @version 1.8 
+*/ 
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
