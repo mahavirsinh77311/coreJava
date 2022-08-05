e5c94b80cbf7dda042d2a44e3641c26d6d0aa58d
mahavirsinh77311 <110014835+mahavirsinh77311@users.noreply.github.com>
2022-08-05 06:12:44 -0700

    Add files via upload

---
+++ RemoveDuplicatesFromList.java
@@ -0,0 +1,31 @@
+package com.topics.coreJava;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+/** 
+ * Represents the List of String
+ * Duplicate String values are removed using LinkedHashSet.
+ * @author mahavirsinh parmar
+ * @version 1.8
+ */
+public class RemoveDuplicatesFromList {
+
+    public static void main(String[] args) {
+        List < String > list = new ArrayList < String > ();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("A");
+        list.add("C");
+        list.add("E");
+        list.add("E");
+        list.add("E");
+        System.out.println("The list is defined as: " + list.toString());
+        Set < String > resultSet = new LinkedHashSet < String > (list);
+        System.out.println("The list after removing the duplicates: " + resultSet);
+    }
+
+}
